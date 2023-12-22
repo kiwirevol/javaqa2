cb00f9969df10ce2d5828550c5cd8cfe5604b2c6
kiwirevol <sukhopara.nikita@yandex.ru>
2023-12-22 23:48:36 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int InitAmount = 100;
+        int TopUpSum = 1100;
+
+        int BonusRuble;
+        if (TopUpSum > 1000) {
+            BonusRuble = TopUpSum / 100;
+        } else {
+            BonusRuble = 0;
+        }
+        int FinalAmount = InitAmount + TopUpSum + BonusRuble;
+
+        System.out.println("Итоговый счёт:" + FinalAmount);
+        System.out.println("Количество бонусных рублей:" + BonusRuble);
+    }
 }
